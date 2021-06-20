521e06794cba20c5aca30abdcdecee8e91007491
thegirlwiththesquirrel <52179804+danaesav@users.noreply.github.com>
2021-06-20 19:41:41 +0300

    Add files via upload

---
+++ Temperature/All-in-one/my_accessory.c
@@ -0,0 +1,59 @@
+#include <homekit/homekit.h>
+#include <homekit/characteristics.h>
+
+// Called to identify this accessory. See HAP section 6.7.6 Identify Routine
+// Generally this is called when paired successfully or click the "Identify Accessory" button in Home APP.
+void my_accessory_identify(homekit_value_t _value) {
+  printf("accessory identify\n");
+}
+
+homekit_characteristic_t cha_current_temperature = HOMEKIT_CHARACTERISTIC_(CURRENT_TEMPERATURE, 1);
+homekit_characteristic_t cha_humidity = HOMEKIT_CHARACTERISTIC_(CURRENT_RELATIVE_HUMIDITY, 1);
+
+homekit_accessory_t *accessories[] = {
+    HOMEKIT_ACCESSORY(.id=1, .category=homekit_accessory_category_bridge, .services=(homekit_service_t*[]) {
+      HOMEKIT_SERVICE(ACCESSORY_INFORMATION, .characteristics=(homekit_characteristic_t*[]) {
+            HOMEKIT_CHARACTERISTIC(NAME, "Multiple Sensors"),
+            HOMEKIT_CHARACTERISTIC(MANUFACTURER, "Danae's Arduino HomeKit"),
+            HOMEKIT_CHARACTERISTIC(SERIAL_NUMBER, "0194726"),
+            HOMEKIT_CHARACTERISTIC(MODEL, "ESP8266"),
+            HOMEKIT_CHARACTERISTIC(FIRMWARE_REVISION, "1.2"),
+            HOMEKIT_CHARACTERISTIC(IDENTIFY, my_accessory_identify),
+            NULL
+        }),
+        NULL
+    }),
+  HOMEKIT_ACCESSORY(.id=2, .category=homekit_accessory_category_sensor, .services=(homekit_service_t*[]) {
+      HOMEKIT_SERVICE(ACCESSORY_INFORMATION, .characteristics=(homekit_characteristic_t*[]) {
+      HOMEKIT_CHARACTERISTIC(NAME, "Temperature Sensor"),
+      HOMEKIT_CHARACTERISTIC(IDENTIFY, my_accessory_identify),
+      NULL
+    }),
+      HOMEKIT_SERVICE(TEMPERATURE_SENSOR, .primary=true, .characteristics=(homekit_characteristic_t*[]) {
+      HOMEKIT_CHARACTERISTIC(NAME, "Temperature"),
+      &cha_current_temperature,
+      NULL
+    }),
+    NULL
+  }),
+  HOMEKIT_ACCESSORY(.id=3, .category=homekit_accessory_category_sensor, .services=(homekit_service_t*[]) {
+      HOMEKIT_SERVICE(ACCESSORY_INFORMATION, .characteristics=(homekit_characteristic_t*[]) {
+      HOMEKIT_CHARACTERISTIC(NAME, "Humidity Sensor"),
+      HOMEKIT_CHARACTERISTIC(IDENTIFY, my_accessory_identify),
+      NULL
+    }),
+      HOMEKIT_SERVICE(HUMIDITY_SENSOR, .primary=true, .characteristics=(homekit_characteristic_t*[]) {
+      HOMEKIT_CHARACTERISTIC(NAME, "Humidity"),
+      &cha_humidity,
+      NULL
+    }),
+    NULL
+  }),
+  
+    NULL
+};
+
+homekit_server_config_t config = {
+    .accessories = accessories,
+    .password = "111-11-111"
+};
